1ba2844264c0499d47b0c6527492524a6ac231ce
Makoorr <azizbenrejeb@gmail.com>
2025-02-01 15:17:21 +0100

    updating profiletype section UI

--- src/components/Job.tsx
+++ src/components/Job.tsx
@@ -1,15 +1,18 @@
 export function Job(props: JobProps) {
     return (
-      <div className="rounded p-6 bg-gray-100 h-full flex flex-col w-96">
-          <h1 className="text-center text-sky-400 font-bold text-lg">
-            {props.title}
-          </h1>
-          <h2 className="text-center pb-4">{props.role}</h2>
-          <div className="text-sm pb-2">{props.description}</div>
-          <div className="grid place-content-center mt-auto">
-            <button className="bg-gradient-to-r from-blue-500 to-cyan-500 text-white px-4 py-2 mt-4">
+      <div className="rounded-xl py-4 px-9 bg-gray-100 h-full flex flex-col w-[26rem]">
+            <div className="text-center pb-4 -space-y-2">
+            <h1 className="text-sky-400 font-bold text-lg">
+              {props.title}
+            </h1>
+            <h2 className="text-lg">{props.role}</h2>
+            </div>
+          <div className="pb-2">{props.description}</div>
+          <div className="flex justify-center mt-auto">
+            <button className="bg-gradient-to-r from-[#8c52ff] to-[#5ce1e6] text-white px-1 py-2 text-lg font-bold transition-colors hover:brightness-110">
               Je suis intéressé(e)
             </button>
+            <div className="bg-[#cbc9cf] w-1/5"></div>
           </div>
       </div>
     );
--- src/pages/sections/ProfileType.tsx
+++ src/pages/sections/ProfileType.tsx
@@ -3,13 +3,13 @@ import { Job } from "../../components/Job";
 export function ProfileType() {
     return (
       <div>
-        <div className="sm:px-20 px-5 py-5 mb-4 w-full">
-          <h1 className="text-3xl font-bold mb-8">
+        <div className="sm:px-40 px-5 py-5 mb-4 w-full">
+          <h1 className="text-3xl font-bold mb-6">
             (H2) Les types de <span className="text-orange-500">profils</span>
           </h1>
-          <h2 className="text-lg">
-          EsaLink vous propose l’expertise de consultants hautement qualifiés dans des domaines à fortes compétences spécifiques, rares et recherchées sur le marché. Nos équipes sont spécialisées dans quatre domaines clés, chacun offrant un accompagnement adapté pour répondre précisément à vos besoins :
-          </h2>
+          <p className="text-lg leading-5">
+            EsaLink vous propose l'expertise de consultants hautement qualifiés dans des domaines à fortes compétences spécifiques, rares et recherchées sur le marché. Nos équipes sont spécialisées dans quatre domaines clés, chacun offrant un accompagnement adapté pour répondre précisément à vos besoins :
+          </p>
         </div>
         <div className="flex py-4">
           <div className="flex flex-col space-y-3 w-1/4 pr-3">
@@ -31,15 +31,7 @@ export function ProfileType() {
               role="Expert - PMO - Manager"
               description={
                 <p>
-                  lorem ipsum dolor sit amet, consectetur adipiscing elit. sed do
-                  eiusmod tempor incididunt ut labore et dolore magna aliqua. ut
-                  enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                  <strong>lorem ipsum dolor sit amet</strong>
-                  nisi ut aliquip ex ea commodo consequat. duis aute irure dolor
-                  in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                  nulla pariatur. excepteur sint occaecat cupidatat non proident,
-                  sunt in culpa qui officia deserunt mollit anim id est laborum.
-                  qui officia deserunt mollit anim id est laborum.
+                  Expert chargé d'<strong>optimiser la gestion des flux logistiques</strong> d'une entreprise, depuis l'approvisionnement en matières premières jusqu'à la livraison des produits finis. Son rôle consiste à analyser les processus existants, identifier des pistes d'amélioration et mettre en œuvre des solutions pour réduire les coûts, améliorer la qualité et augmenter l'efficacité. Grâce à son expertise, il aide les entreprises à mieux gérer leurs ressources, à anticiper les risques et à répondre aux fluctuations du marché.
                 </p>
               }
             />
@@ -48,17 +40,9 @@ export function ProfileType() {
               role="ERP - MM - FICO"
               description={
                 <p>
-                  lorem ipsum dolor sit amet, consectetur adipiscing elit. sed do
-                  eiusmod tempor incididunt ut labore et dolore magna aliqua. ut
-                  enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                  nisi ut aliquip ex ea commodo consequat. duis aute irure dolor
-                  in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                  nulla pariatur. excepteur sint occaecat cupidatat non proident,
-                  sunt in culpa qui officia deserunt mollit anim id est laborum.
-                  <strong>lorem ipsum dolor sit amet</strong>
-                  consectetur adipiscing elit. sed do eiusmod tempor incididunt ut
-                  excepteur sint occaecat cupidatat non proident, sunt in culpa
-                  qui officia deserunt mollit anim id est laborum.
+                  Expert spécialisé dans la mise en œuvre et l'<strong>optimisation des modules du logiciel SAP</strong>, un progiciel de gestion intégré (ERP) utilisé par de nombreuses entreprises pour centraliser leur processus.<br></br> 
+                  Il analyse les besoins de l'entreprise, configure les modules SAP et forme les équipes à leur utilisation.<br></br> 
+                  Grâce à ses compétences, le consultant SAP permet aux entreprises d'améliorer leur efficacité opérationnelle, de mieux gérer leurs ressources et de faciliter la prise de décision grâce à des données fiables et centralisées.
                 </p>
               }
             />
@@ -70,23 +54,23 @@ export function ProfileType() {
   
 function Chip(props: ChipProps) {
   const colors = [
-    "bg-[#5bb6db]/50 sm:w-56 w-36", // Supply chain
-    "bg-[#00b7ff]/50 sm:w-60 w-28", // Distribution
-    "bg-[#4b39b5]/50 text-center pr-5", // SAP
+    "bg-[#5bb6db]/50 sm:w-56 w-36 -translate-x-2", // Supply chain
+    "bg-[#00b7ff]/50 sm:w-60 w-28 -translate-x-2", // Distribution
+    "bg-[#4b39b5]/50 text-center pr-5 -translate-x-2", // SAP
     "bg-[#ff914d]/50 text-center ml-1 sm:w-40 w-24", // SAP MM
-    "from-[#8c52ff]/50 to-[#5ce1e6]/50 text-center ml-1.5 sm:w-52 w-28", // SAP S/4HANA
+    "from-[#8c52ff]/50 to-[#5ce1e6]/50 text-center ml-1.5 sm:w-60 w-28", // SAP S/4HANA
     "bg-[#4b39b5]/50 text-center ml-10 pr-5", // SAP SD
-    "from-[#8c52ff]/50 to-[#5ce1e6]/50 text-center sm:w-52 w-28", // SAP ERP
-    "bg-[#00b7ff]/50 sm:w-48 w-28", // Salesforce
+    "from-[#8c52ff]/50 to-[#5ce1e6]/50 text-center sm:w-52 w-28 -translate-x-2", // SAP ERP
+    "bg-[#00b7ff]/50 sm:w-48 w-28 -translate-x-2", // Salesforce
   ];
   const color = colors[(props.idx - 1) % colors.length];
 
   return (
-    <div className="">
+    <div className="overflow-hidden">
       <span
         className={`
           hover:translate-x-8 inline-block duration-300 transform cursor-pointer
-          rounded-xl bg-gradient-to-r px-4 font-semibold py-2 sm:text-2xl text-lg text-white ${color}
+          rounded-xl bg-gradient-to-r px-4 font-semibold py-1 sm:text-2xl text-lg text-white ${color}
         `}
       >
         {props.title}
